c61c0319fba7d5f33778bc6d82a2420448f08fa9
Subhsis-Padhy-2004 <subhsispadhy@gmail.com>
2026-05-04 21:46:57 +0530

    print  formate

--- Basics.py
+++ Basics.py
@@ -1,1 +1,29 @@
 print("Hello World")
+
+# variable
+monday = 50
+tuesday = 65
+
+avg = (monday+tuesday)/2
+
+print(avg)
+
+# input function
+first_name = input ("enter your first name : ")
+last_name = input ("enter your last name : ")
+
+print(first_name,last_name)
+
+# data type conversion
+
+age = int(input("enter your age"))
+print (age)
+print (age,type(age))
+
+# how to separately print input
+
+full_name = "subhasis"
+ages = 22
+address = "odisha"
+
+print(full_name,ages,address,sep="-")
---
+++ print_formate.py
@@ -0,0 +1,5 @@
+full_name = "subhasis"
+ages = 22
+address = "odisha"
+
+print(full_name,ages,address,sep="-")
