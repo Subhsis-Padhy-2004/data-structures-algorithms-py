326610079d653389aa66fd4f5f61f072b77310ac
Subhsis-Padhy-2004 <subhsispadhy@gmail.com>
2026-05-05 14:07:11 +0530

    nested if else

--- if_else.py
+++ if_else.py
@@ -1,6 +1,11 @@
 age = int(input("Enter your age -->"))
 
-if(age>=18):
-    print ("Eligible for Driving License")
+if age >= 18:
+    # nested if_else condition
+    test = input()
+    if test == "PASS":
+        print("Eligible for Driving License")
+    else:
+        print("Not Eligible for Driving License")
 else:
-    print ("Not Eligible for Driving License")
+    print("Not Eligible for Driving License")
