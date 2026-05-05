00fbb384c755f7084d85fb86a25e1abc4663265e
Subhsis-Padhy-2004 <subhsispadhy@gmail.com>
2026-05-05 16:08:34 +0530

    useing range

---
+++ range.py
@@ -0,0 +1,18 @@
+for k in range(1,11):
+    print(k,end=" ")
+print()
+
+for k in range(1,11,2):
+    print(k,end=" ")
+print()
+
+for k in range(20,0):
+    print(k,end=" ")
+print()
+
+for k in range(20,0,-2):
+    print(k,end=" ")
+print()
+
+
+
